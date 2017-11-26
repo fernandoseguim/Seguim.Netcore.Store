0233d2ecbc7559354e07752cb6ad4444e9d75ec9
Fernando Seguim <fernando.seguim@gmail.com>
2017-11-26 20:49:58 -0200

    create entity product

---
+++ Seguim.Netcore.Store.Domain/StoreContext/Product.cs
@@ -0,0 +1,15 @@
+namespace Seguim.Netcore.Store.Domain.StoreContext
+{
+    public class Product
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public string Price { get; set; }
+
+        public string Quantity { get; set; }
+    }
+}
