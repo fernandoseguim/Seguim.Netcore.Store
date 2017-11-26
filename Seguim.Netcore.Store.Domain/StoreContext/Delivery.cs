c5ba45b83927f81e2708a6eb121e901ad0c86266
Fernando Seguim <fernando.seguim@gmail.com>
2017-11-26 20:50:24 -0200

    create entity delivery

---
+++ Seguim.Netcore.Store.Domain/StoreContext/Delivery.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Seguim.Netcore.Store.Domain.StoreContext
+{
+    public class Delivery
+    {
+        public DateTime CreateDate { get; set; }
+        public DateTime EstimatedDeliveryDate { get; set; }
+        public string Status { get; set; }
+    }
+}
