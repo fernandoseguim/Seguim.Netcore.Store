dfb7fe5fda2816183376c9d4511c336bec519d5f
Fernando Seguim <fernando.seguim@gmail.com>
2017-11-26 23:44:45 -0200

    refactor entity order item - SPOF principle

--- Seguim.Netcore.Store.Domain/StoreContext/Entities/OrderItem.cs
+++ Seguim.Netcore.Store.Domain/StoreContext/Entities/OrderItem.cs
@@ -1,9 +1,13 @@
-namespace Seguim.Netcore.Store.Domain.StoreContext.Entities
-{
-    public class OrderItem
-    {
-        public Product Product { get; set; }
-        public string Quantity { get; set; }
-        public string Price { get; set; }
+namespace Seguim.Netcore.Store.Domain.StoreContext.Entities {
+    public class OrderItem {
+        
+        public OrderItem (Product product, decimal quantity) {
+            this.Product = product;
+            this.Quantity = quantity;
+        }
+
+        public Product Product { get; private set; }
+        public decimal Quantity { get; private set; }
+        public decimal Price { get; private set; }
     }
 }
