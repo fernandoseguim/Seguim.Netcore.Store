cc695cb96f1ee2f52284d8f40785d4cc4aa1ed33
Fernando Seguim <fernando.seguim@gmail.com>
2017-11-26 23:43:58 -0200

    refactor entity product - SPOF principle

--- Seguim.Netcore.Store.Domain/StoreContext/Entities/Product.cs
+++ Seguim.Netcore.Store.Domain/StoreContext/Entities/Product.cs
@@ -1,15 +1,21 @@
-namespace Seguim.Netcore.Store.Domain.StoreContext.Entities
-{
-    public class Product
-    {
-        public string Title { get; set; }
+namespace Seguim.Netcore.Store.Domain.StoreContext.Entities {
+    public class Product {
+        public Product (string title, string description, string image, decimal price, decimal quantity) {
+            this.Title = title;
+            this.Description = description;
+            this.Image = image;
+            this.Price = price;
+            this.QuantityOnHand = quantity;
 
-        public string Description { get; set; }
+        }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal QuantityOnHand { get; private set; }
 
-        public string Image { get; set; }
-
-        public string Price { get; set; }
-
-        public string Quantity { get; set; }
+        public override string ToString () {
+            return this.Title;
+        }
     }
 }
