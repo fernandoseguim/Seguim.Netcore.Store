90359a3537e053fabd557499a0626ede144dfb6e
Fernando Seguim <fernando.seguim@gmail.com>
2017-11-26 23:45:03 -0200

    refactor entity Order  - SPOF principle

--- Seguim.Netcore.Store.Domain/StoreContext/Entities/Order.cs
+++ Seguim.Netcore.Store.Domain/StoreContext/Entities/Order.cs
@@ -1,20 +1,33 @@
 using System;
 using System.Collections.Generic;
+using Seguim.Netcore.Store.Domain.StoreContext.Enums;
 
-namespace Seguim.Netcore.Store.Domain.StoreContext.Entities
-{
+namespace Seguim.Netcore.Store.Domain.StoreContext.Entities {
 
     public class Order {
-        public Customer Customer { get; set; }
-        public string Number { get; set; }
-        public DateTime CreateDate { get; set; }
-        public string Status { get; set; }
-        public IList<OrderItem> Items { get; set; }
-        public IList<Delivery> Deliveries { get; set; }
+        public Order (Customer customer) {
+            this.Customer = customer;
+            this.Number = Guid.NewGuid().ToString().Replace("-","").Substring(0,8).ToUpper();
+            this.CreateDate = DateTime.Now;
+            this.Status = EOrderStatus.Created;
+            Items = new List<OrderItem>();
+            Deliveries = new List<Delivery>();
+        }
+
+        public Customer Customer { get; private set; }
+        public string Number { get; private set; }
+        public DateTime CreateDate { get; private set; }
+        public EOrderStatus Status { get; private set; }
+        public IReadOnlyCollection<OrderItem> Items { get; private set; }
+        public IReadOnlyCollection<Delivery> Deliveries { get; private set; }
+
+        public void AddItem(OrderItem orderItem)
+        {
+            // Valida o item
+            // Adiciona ao pedido
+        }
 
         // To Place an Order
-        public void Place () {
-
-        }
+        public void Place () { }
     }
 }
