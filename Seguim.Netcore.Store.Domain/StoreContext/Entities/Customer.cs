4995698f0973808feb3239ad5901d21fa6383626
Fernando Seguim <fernando.seguim@gmail.com>
2017-11-26 23:43:21 -0200

    refactor entity customer - SPOF principle

--- Seguim.Netcore.Store.Domain/StoreContext/Entities/Customer.cs
+++ Seguim.Netcore.Store.Domain/StoreContext/Entities/Customer.cs
@@ -1,15 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using Seguim.Netcore.Store.Domain.StoreContext.ValueObjects;
 
-namespace Seguim.Netcore.Store.Domain.StoreContext.Entities
-{
-    public class Customer 
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Document { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
+namespace Seguim.Netcore.Store.Domain.StoreContext.Entities {
 
+    public class Customer {
+        public Customer (Name name, Document document, Email email, string phone) 
+        {
+            Name = name;
+            Document = document;
+            Email = email;
+            Phone = phone;
+            Addresses = new List<Address>();
+        }
+
+        public Name Name { get; private set; }
+        
+        public Document Document { get; private set; }
+        public Email Email { get; private set; }
+        public string Phone { get; private set; }
+        public IReadOnlyCollection<Address> Addresses { get; private set; }
+
+        public void AddAddress(Address address){
+
+            //Adiciona endereço
+        }
+
+        public override string ToString()
+        {
+            return Name.ToString();
+        }
     }
 }
